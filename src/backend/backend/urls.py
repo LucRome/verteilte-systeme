802e99b0d9a267570e1078355c6f9e01989aa9b2
LucRome <64947931+LucRome@users.noreply.github.com>
2022-05-03 08:44:30 +0200

    add pagination to person list

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -21,5 +21,5 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
-    path('/', include('birthday_db.urls')),
+    path('persons/', include('birthday_db.urls')),
 ]
